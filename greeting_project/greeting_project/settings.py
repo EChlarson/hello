b25c17e2f244173c7890be9162c2878a172bd72f
EChlarson <chl20001@byui.edu>
2026-02-21 17:19:56 -0600

    Trouble Shooting Error

--- greeting_project/greeting_project/settings.py
+++ greeting_project/greeting_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-ucxvjd*fjlycb8wup&x1m2+g3=1np)llt6&60waedmsdp@l^gk')
+SECRET_KEY = 'django-insecure-ucxvjd*fjlycb8wup&x1m2+g3=1np)llt6&60waedmsdp@l^gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['*']
 
